8eaa6dd408cec5545f3443072d9af19cbcbf8343
Tim Baz <timtb.dev@ggmail.com>
2025-11-28 05:37:29 -0800

    Fixed: linting issues

--- reset.d.ts
+++
@@ -1,4 +0,0 @@
-// Provides stricter TypeScript types for common operations like .filter(Boolean),
-// JSON.parse, and array methods. This makes TypeScript catch more bugs at compile time.
-// See: https://github.com/total-typescript/ts-reset
-import "@total-typescript/ts-reset";
