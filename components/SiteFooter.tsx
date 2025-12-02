49ca19179d124677f70957ce9f096b76d11f2025
Tim Baz <timtb.dev@ggmail.com>
2025-12-02 04:59:34 -0800

    Fixed: Footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,12 +2,14 @@ import TechStacks from "@/components/footer/TechStacks";
 import BottomLinks from "@/components/footer/BottomLinks";
 import InspiredBy from "@/components/footer/InspiredBy";
 import Copyright from "@/components/footer/Copyright";
+import SeparatorHorizontal from "@/components/SeparatorHorizontal";
 
 export function SiteFooter() {
   return (
     <footer className="w-full mx-auto overflow-x-hidden">
       <InspiredBy />
       <TechStacks />
+      <SeparatorHorizontal />
       <BottomLinks />
       <Copyright />
     </footer>
